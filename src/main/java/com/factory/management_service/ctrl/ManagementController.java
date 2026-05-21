d2f147cb6baa9f4da19ceae74f1c12b6c83745a7
unknown <lina083017@gmail.com>
2026-05-21 09:55:51 +0900

    chore: db 수정, grafana API 안씀, dev.yml 추가

--- src/main/java/com/factory/management_service/ctrl/ManagementController.java
+++ src/main/java/com/factory/management_service/ctrl/ManagementController.java
@@ -10,7 +10,6 @@ import com.factory.management_service.service.ManagementService;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
--- src/main/java/com/factory/management_service/dao/MasterRecipeRepository.java
+++ src/main/java/com/factory/management_service/dao/MasterRecipeRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.factory.management_service.domain.entity.MasterRecipeEntity;
 
-public interface MasterRecipeRepository extends JpaRepository<MasterRecipeEntity, String> {
+public interface MasterRecipeRepository extends JpaRepository<MasterRecipeEntity, Long> {
 
 }
