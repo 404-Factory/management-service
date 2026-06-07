898c0949ecfa0db76a988069df0d11538ebffe15
박명환 <jackpark5872@yonsei.ac.kr>
2026-06-08 02:08:25 +0900

    refactor: @Repository 추가 및 ManagementServiceApplication JpaRepository 및 EntityScan 추가

--- src/main/java/com/factory/management/ManagementServiceApplication.java
+++ src/main/java/com/factory/management/ManagementServiceApplication.java
@@ -2,10 +2,14 @@ package com.factory.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(basePackages = "com.factory.management")
+@EntityScan(basePackages = "com.factory.management")
 public class ManagementServiceApplication {
 
 	public static void main(String[] args) {
@@ -13,3 +17,5 @@ public class ManagementServiceApplication {
 	}
 
 }
+
+
