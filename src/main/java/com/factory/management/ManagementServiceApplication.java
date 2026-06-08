35bc6f76afd1ee54f37ca516dab7232680b1a8ea
박명환 <jackpark5872@yonsei.ac.kr>
2026-06-08 19:40:44 +0900

    refactor: EnableJpaRepositories/EntityScan 삭제

--- src/main/java/com/factory/management/ManagementServiceApplication.java
+++ src/main/java/com/factory/management/ManagementServiceApplication.java
@@ -2,15 +2,10 @@ package com.factory.management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.core.env.Environment;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@EnableJpaRepositories(basePackages = "com.factory.management")
-@EntityScan(basePackages = "com.factory.management")
 public class ManagementServiceApplication {
 
 	public static void main(String[] args) {
