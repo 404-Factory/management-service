d3d53defc9c5980f490a7d591cff898665aca888
rymph <rymph0501@gmail.com>
2026-06-08 11:39:33 +0900

    refactor: fetch defect utils from main

---
+++ src/main/java/com/factory/management/simulator/util/DefectCreateService.java
@@ -0,0 +1,37 @@
+package com.factory.management.simulator.util;
+
+import org.springframework.stereotype.Service;
+
+import com.factory.management_service.dao.DefectRepository;
+import com.factory.management_service.domain.entity.DefectEntity;
+import com.factory.management_service.kafka.event.DefectCreatedEvent;
+import com.factory.management_service.kafka.producer.DefectEventProducer;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class DefectCreateService {
+
+    private final DefectRepository defectRepository;
+    private final DefectEventProducer defectEventProducer;
+
+    public void create(DefectEntity defectEntity) {
+
+        DefectEntity saved = defectRepository.save(defectEntity);
+
+        DefectCreatedEvent event = DefectCreatedEvent.builder()
+                .defectId(saved.getDefectId())
+                .defectType(saved.getDefectType())
+                .defectCode(saved.getDefectCode())
+                .detectedTime(saved.getDetectedTime())
+                .occurredTime(saved.getOccurredTime())
+                .causeProcessId(saved.getCauseProcessId())
+                .causeProcessName(saved.getCauseProcessName())
+                .causeEquipmentId(saved.getCauseEquipmentId())
+                .causeEquipmentName(saved.getCauseEquipmentName())
+                .build();
+
+        defectEventProducer.publish(event);
+    }
+}
